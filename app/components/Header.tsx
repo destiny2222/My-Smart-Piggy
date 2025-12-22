4d66528631e74e60853bbe143af01d92ac423905
destiny2222 <oghenekumeerieme@gmail.com>
2025-12-22 18:16:42 +0100

    Fix import statement for Button component in Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "./ui/button";
+import  Button  from "./ui/button";
 import { Menu, X, PiggyBank } from "lucide-react";
 import Link from "next/link";
 
